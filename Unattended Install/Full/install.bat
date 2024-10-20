93ac4340d6e6942364af9366c419a461628d3881
L1ttl3F00t <36143389+L1ttl3F00t@users.noreply.github.com>
2024-10-19 20:53:53 -0400

    add check for unattended install
    GPT script cleanup - add a check to see if AHK installed and if script already running from startup folder

--- Unattended Install/Full/install.bat
+++ Unattended Install/Full/install.bat
@@ -1,35 +1,46 @@
 @echo off
 setlocal
 
-REM Set the target directory
-set "targetDir=%CD%\resources"
+REM Define the AutoHotkey installation path and script path
+set "ahkPath=C:\Program Files\AutoHotkey\AutoHotkey.exe"
+set "scriptPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\AHK-master-script.ahk"
 
-REM Create the target directory if it doesn't exist
-if not exist "%targetDir%" mkdir "%targetDir%"
+REM Check if AutoHotkey is already installed
+if exist "%ahkPath%" (
+    echo AutoHotkey is already installed.
+) else (
+    REM Install AutoHotkey silently if not installed
+    call .\AutoHotkey_1.1.37.01_setup.exe /S
+    echo AutoHotkey has been installed.
+)
 
-REM Change to the target directory
-cd "%targetDir%"
+REM Check if the script already exists in the Startup folder
+if exist "%scriptPath%" (
+    echo The AutoHotkey master script is already set up in the Startup folder.
+) else (
+    REM Set the target directory for downloading the script
+    set "targetDir=%CD%\resources"
 
-REM Download Master Script using curl
-curl -o AHK-master-script.ahk -L "https://raw.githubusercontent.com/KuduVault/autohotkey/master/AHK-master-script.ahk"
+    REM Create the target directory if it doesn't exist
+    if not exist "%targetDir%" mkdir "%targetDir%"
 
-REM Install AutoHotkey silently
-call .\AutoHotkey_1.1.37.01_setup.exe /S
+    REM Change to the target directory
+    cd "%targetDir%"
 
-REM Copy AHK-master-script.ahk to the Startup folder
-copy AHK-master-script.ahk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+    REM Download Master Script using curl
+    curl -o AHK-master-script.ahk -L "https://raw.githubusercontent.com/L1ttl3F00t/autohotkey/master/AHK-master-script.ahk"
 
-REM Run AutoHotkey script
-start /MIN "" "C:\Program Files\AutoHotkey\AutoHotkey.exe" "%targetDir%\AHK-master-script.ahk"
+    REM Copy AHK-master-script.ahk to the Startup folder
+    copy AHK-master-script.ahk "%scriptPath%"
+    echo AutoHotkey master script has been set up in the Startup folder.
+
+    REM Run AutoHotkey script
+    start /MIN "" "%ahkPath%" "%scriptPath%"
+    echo AutoHotkey master script is now running.
+)
 
 REM Wait for a moment before exiting
 timeout /nobreak /t 3 >nul
-
-REM Open Image
-completion.png
-
-REM Go to Sarielnet
-start "" http://pwn.sariel.net
 
 REM Exit the script
 exit /b 0
